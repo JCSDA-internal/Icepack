85c16690621798b921aa5a8328e860f7de1f2762
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2022-06-29 11:58:31 -0400

    added more preproc defs

--- cmake/icepack_compiler_flags.cmake
+++ cmake/icepack_compiler_flags.cmake
@@ -3,7 +3,7 @@
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
-add_definitions ( -Duse_libMPI -Duse_netCDF -DSPMD -DNXGLOB=4 -DNICELYR=7 -DNSNWLYR=1 -DNICECAT=5 -DTRAGE=1 -DTRFY=1 -DTRLVL=1 -DTRPND=1 -DTRBRI=0 -DNTRAERO=1  -DTRZS=0 -DNBGCLYR=7 -DTRALG=0 -DTRBGCZ=0 -DTRDOC=0 -DTRDOC=0 -DTRDIC=0 -DTRDON=0 -DTRFED=0 -DTRFEP=0 -DTRZAERO=0 -DTRBGCS=0 -DNFSDCAT=1 )
+add_definitions ( -Duse_libMPI -Duse_netCDF -DSPMD -DNXGLOB=4 -DNICELYR=7 -DNSNWLYR=1 -DNICECAT=5 -DTRAGE=1 -DTRFY=1 -DTRLVL=1 -DTRPND=1 -DTRBRI=0 -DNTRAERO=1  -DTRZS=0 -DNBGCLYR=7 -DTRALG=0 -DTRBGCZ=0 -DTRDOC=0 -DTRDOC=0 -DTRDIC=0 -DTRDON=0 -DTRFED=0 -DTRFEP=0 -DTRZAERO=0 -DTRBGCS=0 -DNFSDCAT=1 -DNTRISO=3 -DTRSNOW=1 )
 
 #######################################################################################
 # Fortran
